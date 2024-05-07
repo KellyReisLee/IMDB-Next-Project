bb4b57a31fc64136fcc2c03d36eecf63b9f5ba6c
KellyReisLee <kristinaalvares28@gmail.com>
2024-05-07 15:50:40 +0100

    fetch data from an API

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,28 @@
+import MoviesItem from '@/components/MoviesItem';
 import React from 'react'
 
-const Home = () => {
+const API_KEY = process.env.API_KEY;
+
+const Home = async ({ searchParams }) => {
+
+  const genre = searchParams.genre || 'fetchTrending';
+  const res = await fetch(`https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`)
+
+  const resData = await res.json()
+  if (!res.ok) {
+    throw new Error('Failed to fetch data.')
+  }
+
+  const moviesData = resData.results
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='p-8 h-screen'>
+      {
+        moviesData.map((movie) => (
+          <MoviesItem key={movie.id} moviesData={movie} />
+        ))
+      }
 
     </div>
   )

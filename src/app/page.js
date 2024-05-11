4b6238f84b99ef4b83130ec62ffc7b2a2e9eb97e
KellyReisLee <kristinaalvares28@gmail.com>
2024-05-11 15:12:01 +0100

    wrapping with Suspense

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import Boxes from '@/components/Boxes';
-import Card from '@/components/Card';
-import React from 'react'
+import React, { Suspense } from 'react'
 
 
 
@@ -22,11 +21,11 @@ const Home = async ({ searchParams }) => {
 
 
   return (
-    <div className='sm:grid  sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4 px-4  gap-3 sm:gap-5'>
-      <Boxes dataList={moviesData} />
-
-
-    </div>
+    <Suspense>
+      <div className='sm:grid  sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4 px-4  gap-3 sm:gap-5'>
+        <Boxes dataList={moviesData} />
+      </div>
+    </Suspense>
   )
 }
 

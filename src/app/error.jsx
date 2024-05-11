69bc5fa1f8bc4196c20e467254761c614cf8beaf
KellyReisLee <kristinaalvares28@gmail.com>
2024-05-11 15:04:36 +0100

    change error page

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,24 +1,16 @@
-'use client'
+'use client';
 
-import { useEffect } from 'react'
-
-const Error = ({ error, reset }) => {
+import { useEffect } from 'react';
+export default function Error({ error, reset }) {
   useEffect(() => {
     console.log(error);
-
-  }, [error])
-
-  const handleReset = () => {
-
-  }
-
+  }, [error]);
   return (
     <div className='text-center mt-10'>
-      <h1> Something wen wrong. Please, try again</h1>
-      <button className='hover:text-gray-600 cursor-pointer dark:bg-amber-500  dark:text-gray-700  px-6 mt-2 py-1 border-2 border-amber-500 rounded' onClick={() => handleReset()}>Try Again</button>
+      <h1>Something went wrong. Please try again later.</h1>
+      <button className='hover:text-amber-600' onClick={() => reset()}>
+        Try Again
+      </button>
     </div>
-  )
+  );
 }
-
-
-export default Error
